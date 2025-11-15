8d2f54e4d5626be29756c57c839ad35390f885a6
Ana Lucía Restrepo <anal.restrepot@uqvirtual.edu.co>
2025-11-15 17:45:05 -0500

    Etapa 1. de exploración de los datos

--- ProyectoFinal.py
+++ ProyectoFinal.py
@@ -3,9 +3,48 @@
 
 
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-#1. Importación de archivos y fuentes de datos
+#1. Importación de archivos y fuentes de datos. Secargó la base de datos
 
-df_csv = pd.read_csv("archivo.csv")  # Leer archivo CSV local
+df= pd.read_csv("Yanacona.csv")
+print(df)
 
-ssssssss
+# 2. Exploración Inicial
+#primeras 5 filas
+
+print(df.head())
+
+#ultimas 5 filas
+print(df.tail())
+
+#dimensiones del data set
+print(df.shape)
+
+#nombre de las columnas
+print(df.columns)
+
+#tipo de datos y valores nulos
+print(df.info())
+
+#estadistica basica
+print(df.describe())
+
+# Estadísticas para columnas categóricas (tipo object o string)
+print("Estadísticas de variables categóricas:")
+print(df.describe(include='object'))
+
+# Conteo de filas duplicadas
+print("Número de filas duplicadas:")
+print(df.duplicated().sum())
+
+# Ver primeras filas duplicadas si existen
+print("Filas duplicadas (si existen):")
+print(df[df.duplicated()].head())
+
+# Si quieres ver la correlación entre variables numéricas:
+print("Correlación entre variables numéricas:")
+print(df.corr(numeric_only=True))
+
+
