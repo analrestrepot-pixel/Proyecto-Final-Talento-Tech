4394c0a6f9b07033c46f4c2be9e43b61fb3903a1
Ana Lucía Restrepo <anal.restrepot@uqvirtual.edu.co>
2025-11-16 00:24:03 -0500

    Reapply "Reapply "Reapply "3.Parte limpieza de datos (1)"""
    This reverts commit 5e58fdce25010e8b3b9d5b3eef6e30716b864c3c.

--- ProyectoFinal.py
+++ ProyectoFinal.py
@@ -1,7 +1,3 @@
-
-
-
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,7 +9,6 @@ print(df)
 
 # 2. Exploración Inicial
 #primeras 5 filas
-
 print(df.head())
 
 #ultimas 5 filas
@@ -47,4 +42,35 @@ print(df[df.duplicated()].head())
 print("Correlación entre variables numéricas:")
 print(df.corr(numeric_only=True))
 
+#▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰  3. Limpieza de Datos ▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+# valores Nulos
+print(df.isnull().sum())
 
+# Ver porcentaje de nulos por columna
+print(df.isnull().mean() * 100)
+
+# Eliminamos columnas no útiles para análisis inicial
+df.drop(columns=['CC', 'NUMERO DOCUMENTO', 'TELEFONO'], inplace=True)
+
+
+# Se convirtio columna sexo de tipo object a category
+df['SEXO']=df ['SEXO'].astype('category')
+print(df.info())
+
+moda = df['DESPLAZ'].mode()
+df['DESPLAZ']= df['DESPLAZ'].str.upper()
+df.loc[df['DESPLAZ'] == 'P', 'DESPLAZ'] = moda
+# Eliminamos columnas no útiles para análisis inicial
+df.drop(columns=['columna'], inplace=True)
+
+# Imputación de valores nulos en variables seleccionadas
+# 'Age': usamos la mediana porque es robusta ante outliers
+df['DESPLAZ']=df ['DESPLAZ'].astype('category')
+
+#'Embarked': usamos la moda (valor más frecuente) para conservar la categoría dominante
+df['DESPLAZ'].fillna(df['DESPLAZ'].mode()[0], inplace=True)  # Moda para Embarque
+print(df.isnull().sum())
+df['DESPLAZ'].mode()
+
+
+print(df.info())
