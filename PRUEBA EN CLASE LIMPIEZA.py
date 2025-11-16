1a5604d9e40fa8994dcf74a9632b62e3b55c3b3a
Ana Lucía Restrepo <anal.restrepot@uqvirtual.edu.co>
2025-11-16 12:12:09 -0500

    Renombrar las Variables Finalizado

--- PRUEBA EN CLASE LIMPIEZA.py
+++ PRUEBA EN CLASE LIMPIEZA.py
@@ -24,28 +24,52 @@ df.drop(columns=['Grupo Etario', 'FechaCorte', 'FechaActualizacion',
                  'Posee Cónyuge o Compañero(a)?','Máximo Nivel FpT Reportado',
                  'Línea de FpT para el Máx. Nivel','N° de Hijos','Estado ISUN',
                 'Estado de la vinculación ASS','Tipo de BIE Accedido',
-                'DesagregadoDesembolsoBIE','OcupacionEconomica'], inplace=True)
-#Cambiar Nombres
+                'DesagregadoDesembolsoBIE','OcupacionEconomica','Posee Censo de Familia?'], inplace=True)
 
-df.rename (columns={"Tipo de Desmovilización": "tipo_desmovilizacion"}, inplace=True) # se cambia Fare por tarifa
+#Cambiar Nombres de los encabezados de las Columnas y asignación de los nombbres de las variables
+
+df.rename (columns={"Tipo de Desmovilización": "tipo_desmovilizacion"}, inplace=True) 
 print(df.columns)
 
-df.rename (columns={"Ex Grupo": "grupoArmado"}, inplace=True) # se EX grupo por Grupo Armado
+df.rename (columns={"Ex Grupo": "grupoArmado"}, inplace=True)
 print(df.columns)
 
-df.rename (columns={"Año desmovilización": "anoDesmov"}, inplace=True) # se EX grupo por Grupo Armado
+df.rename (columns={"Año desmovilización": "anoDesmov"}, inplace=True) 
 print(df.columns)
 
-df.rename (columns={"Año desmovilización": "anoDesmov"}, inplace=True) # se EX grupo por Grupo Armado
+df.rename (columns={"Sexo": "sexo"}, inplace=True) 
 print(df.columns)
-ntr
+
+df.rename (columns={"Situación Final frente al proceso": "estadoProceso"}, inplace=True) 
+print(df.columns)
+
+df.rename (columns={"Régimen de salud": "eps"}, inplace=True) 
+print(df.columns)
+
+df.rename (columns={"Posee Serv. Públicos Básicos": "servPublicos"}, inplace=True) 
+print(df.columns)
+
+df.rename (columns={"Departamento de residencia descripción": "departamento"}, inplace=True)
+print(df.columns)
+
+df.rename (columns={"Desembolso BIE": "subsidiado"}, inplace=True) 
+print(df.columns)
+
+df.rename (columns={"Total Integrantes grupo familiar": "integrantesFamilia"}, inplace=True)
+print(df.columns)
+
+df.rename (columns={"Posee Censo de Habitabilidad?": "censoVivienda"}, inplace=True) 
+print(df.columns)
 
 
+df.rename (columns={"Ingresó/No ingresó": "ingresoSub"}, inplace=True) 
+print(df.columns)
 
+df.rename (columns={"Año de Independización/Ingreso": "anoIngreso"}, inplace=True) 
+print(df.columns)
 
+df.rename (columns={"Departamento de residencia": "codigoDepartamento"}, inplace=True) 
+print(df.columns)
 
-
-
-
-
-
+df.rename (columns={"Nivel Educativo": "nivelEducativo"}, inplace=True) 
+print(df.columns)
